98147d3575de1dbeaa95d74fe746966ddd839a01
Steve Tripp <steven.m.tripp@gmail.com>
2025-01-28 16:27:02 -0700

    Add Splash Screen and update dependencies
    This commit introduces a splash screen to the application and updates several dependencies.
    
    -   Added the `androidx.core:core-splashscreen` dependency.
    -   Implemented the `SplashScreen` API in `MainActivity` using `installSplashScreen()`.
    -   Created a custom splash screen theme, `AppTheme.Splash`, in `themes.xml` that sets the background color and animated icon.
    -   Replaced `ic_launcher_background.xml` with a solid white background for the splash screen.
    - Added a new icon `ic_launcher_foreground.xml` for splash screen
    -   Updated the `AndroidManifest.xml` to use `AppTheme.Splash` as the activity theme.
    - Updated Hilt dependency to version 2.55.
    -   Removed the old `Theme.RegexWordFinder` theme.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.splashscreen)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
--- app/src/main/java/com/trippntechnology/regexwordfinder/ux/mainactivity/MainActivity.kt
+++ app/src/main/java/com/trippntechnology/regexwordfinder/ux/mainactivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.trippntechnology.regexwordfinder.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme { MainActivityScreen() }
